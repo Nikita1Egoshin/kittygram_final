e8affee78c1d2eac8744859c6143ae52b03eae5e
Никита Егошин <egoshinn@mail.ru>
2025-09-04 01:48:30 +0500

    ver 1.4

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://koshkinsite.sytes.net/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'koshkinsite.sytes.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
